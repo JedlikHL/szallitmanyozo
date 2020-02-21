aa332df59c0ff0d5382ae559c01fe2c4055d997f
Hoffmann.László <Hoffmann.Lszl@C102-02>
2020-02-21 09:03:42 +0100

    Az adatszerkezet elkészült

---
+++ szallitmanyozo/szallitmanyozo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szallitmanyozo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] teljes = { "ABC-123", "Asd-234", "FGH-432", "BAB-987", "SEX-444" };
+            List<string> bent = new List<string>();
+            Console.ReadKey();
+        }
+    }
+}
