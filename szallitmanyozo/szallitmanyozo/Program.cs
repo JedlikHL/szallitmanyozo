bd9b126e28b2849b1ccb789fe2baba6d794b35d6
Hoffmann.László <Hoffmann.Lszl@C102-02>
2020-02-21 09:11:26 +0100

    Alapprogram

--- szallitmanyozo/szallitmanyozo/Program.cs
+++ szallitmanyozo/szallitmanyozo/Program.cs
@@ -12,6 +12,27 @@ namespace szallitmanyozo
         {
             string[] teljes = { "ABC-123", "Asd-234", "FGH-432", "BAB-987", "SEX-444" };
             List<string> bent = new List<string>();
+            do
+            {
+                //A választási lehetőségek kiírása
+                Console.WriteLine("Elmegy (E)");
+                Console.WriteLine("Bejön (B)");
+                Console.WriteLine("Kilépés (K)");
+                //A választás bekéréseú
+                választás = Console.ReadLine();
+                switch (választás)
+                {
+                    case "E":Elmegy();
+                        break;
+                    case "B":Bejön();
+                        break;
+                    case "K":Kilépés();
+                        break;
+                    default: Console.WriteLine("Nem megfelelő választás");
+                        break;
+                }
+
+            } while (választás!="K");
             Console.ReadKey();
         }
     }
