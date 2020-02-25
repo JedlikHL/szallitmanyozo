edc7d935f16d226915cb9f164cf511c166dbcca1
Hoffmann.László <Hoffmann.Lszl@C102-02>
2020-02-25 12:58:30 +0100

    kész progi

--- szallitmanyozo/szallitmanyozo/Program.cs
+++ szallitmanyozo/szallitmanyozo/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace szallitmanyozo
+namespace Szállitmanyozo
 {
     class Program
     {
@@ -13,56 +13,141 @@ namespace szallitmanyozo
             string[] teljes = { "ABC-123", "ASD-234", "FGH-432", "BAB-987", "SEX-444" };
             List<string> bent = new List<string>();
             string választás;
+            // Kiíró eljárás, Melyikek vannak bent jelenleg, Hívd meg az Elmegynél és a Bejönnél
+
             do
             {
-                //A választási lehetőségek kiírása
-                Console.WriteLine("Elmegy (E)");
-                Console.WriteLine("Bejön (B)");
-                Console.WriteLine("Kilépés (K)");
-                //A választás bekéréseú
+                // A választási lehetőségek kiírása
+                Console.WriteLine("Elmegy ('E')");
+                Console.WriteLine("Bejön ('B')");
+                Console.WriteLine("Kilépés ('K')");
+                // Be kell kérni 1 választást
+                Console.Write("Válasszon műveletet: ");
                 választás = Console.ReadLine();
                 switch (választás)
                 {
-                    case "E":Elmegy();
+                    case "E":
+                        Elmegy(választás, bent);
                         break;
-                    case "B":Bejön(teljes,bent);
+                    case "B":
+                        Bejön(választás, bent);
                         break;
-                    case "K":Kilépés();
-                       break;
-                    default: Console.WriteLine("Nem megfelelő választás");
+                    case "K":
+                        Kilépés(ref választás);
+                        break;
+                    default:
+                        Console.WriteLine("Nem megfelelő a választás");
                         break;
                 }
+            } while (választás != "K"); // K = Kilépés 
 
-            } while (választás!="K");
             Console.ReadKey();
         }
-
-        private static void Kilépés()
+        static void Elmegy(string választás, List<string> bent)
         {
-            
-        }
-
-        private static void Elmegy()
-        {
-            //Ha üres a lista, akkor írjuk ki, hzogy nincs bent autó
-            //Ellenőrzés, hogy az autó a listában van-e
-            //Ha igen, akkor ki kell törölni
-        }
-
-        static void Bejön(string[] teljes, List<string> bent)
-        {
-            if (bent.Count)
+            if (bent.Count == 0)
             {
-
+                Kiíró(bent);
+                Console.ReadKey();
+                Console.Clear();
             }
-            Console.WriteLine("Adja meg a rendszámot:");
+            else
+            {
+                Console.Write("Rendszám: ");
+                string rendszám = Console.ReadLine();
+                for (int i = 0; i < bent.Count; i++)
+                {
+                    if (rendszám == bent[i])
+                    {
+                        bent.Remove(rendszám);
+                        Console.WriteLine("Az autó elment");
+                        Kiíró(bent);
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    }
+                    else if (i == bent.Count - 1)
+                    {
+                        Console.WriteLine("Nincs bent ilyen autó");
+                        Kiíró(bent);
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+
+                }
+            }
+        }
+        static void Bejön(string választás, List<string> bent)
+        {
+            Console.Write("Rendszám: ");
             string rendszám = Console.ReadLine();
-            if (teljes.Contains(rendszám))
+            if (bent.Count == 0)
             {
                 bent.Add(rendszám);
-                Console.WriteLine("Engedélyezve");
+                Console.WriteLine("Autó hozzáadva");
+                Kiíró(bent);
+                Console.ReadKey();
+                Console.Clear();
             }
-            else { Console.WriteLine("Ez az autó nem jöhet be, mert nem szerepel a nyilvántartásban!"); }
+            else
+            {
+                for (int i = 0; i < bent.Count; i++)
+                {
+                    if (bent[i] == rendszám)
+                    {
+                        Console.WriteLine("Már van bent ilyen Autó");
+                        Kiíró(bent);
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    }
+                    else
+                    {
+                        bent.Add(rendszám);
+                        Console.WriteLine("Autó hozzáadva");
+                        Kiíró(bent);
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    }
+                }
+            }
+
+        }
+        static string Kilépés(ref string választás)
+        {
+            Console.WriteLine("Biztostan kilép? (igen/nem)");
+            string válasz = Console.ReadLine();
+            if (válasz == "igen")
+            {
+                választás = "K";
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Elmegy (E)");
+                Console.WriteLine("Bejön (B)");
+                Console.WriteLine("Kilépés (K)");
+                választás = Console.ReadLine();
+            }
+            return választás;
+        }
+        static void Kiíró(List<string> bent)
+        {
+            if (bent.Count == 0)
+            {
+                Console.WriteLine("Nincs bent autó");
+            }
+            else
+            {
+                Console.Write("Bent lévő autók: ");
+                foreach (string item in bent)
+                {
+                    Console.Write($"{item}  ");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
